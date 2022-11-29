58f25ca04ea100c989dadfd46ee2bf4cd31d59f0
martin <jmartinaguirre79@gmail.com>
2022-11-29 15:45:34 -0300

    Para clase 3

---
+++ package/package.js
@@ -0,0 +1,22 @@
+import fs from "fs";
+
+const pkg = fs.readFileSync("package.json", "utf-8")
+console.log(pkg)
+
+const info = {
+    conteidoStr: pkg,
+    contenidoObj: JSON.parse(pkg),
+    size: fs.statSync("package.json").size,
+}
+
+console.log(info)
+
+async function writeFile(){
+    try {
+        await fs.promises.writeFileSync("info.json", JSON.stringify(info));
+        const infoJSON  = JSON.parse(await fs.promises.readFile("info.json", "utf-8")) 
+    } catch (error) {
+        console.log(error);
+        throw new Error(error)
+    }
+}
