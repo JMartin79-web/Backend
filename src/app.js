560a179a18053571c7f3e3ed50fb51104d0c1a4f
martin <jmartinaguirre79@gmail.com>
2022-12-12 10:45:06 -0300

    Creando package.json

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT= 8080
+const server = app.listen(PORT, () => console.log(`🚀 Server started on port http://localhost:${PORT}`))
+server.on('error', (err) => console.log(err));
+
