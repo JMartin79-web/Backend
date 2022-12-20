7ba67fad28de3899f9233a01c30d3689dad90c62
martin <jmartinaguirre79@gmail.com>
2022-12-20 16:04:22 -0300

    Aplicando handlebars

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
+import __dirname from './utils.js';
+
 import { productsRouter } from './routes/products.router.js';
 import { cartsRouter } from './routes/carts.router.js';
 
@@ -7,9 +10,25 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.engine("handlebars", handlebars.engine())
+app.set("views", __dirname + "/views")
+app.set("view engine", "handlebars")
+
+app.use("/api/products/", productsRouter);
+app.use("/api/carts/", cartsRouter);
+
+app.use(express.static(__dirname+"/public"))
+
+app.get("/", (req,res)=>{
+    let testUSer = {
+        name: "Martin",
+        last_name: "Aguirre"
+    }
+    res.render("index", testUSer)
+})
+
+
 const PORT = 8080
 const server = app.listen(PORT, () => console.log(`🚀 Server started on port http://localhost:${PORT}`))
 server.on('error', (err) => console.log(err));
 
-app.use("/api/products/", productsRouter);
-app.use("/api/carts/", cartsRouter);
